04da69d07a1821625d767a52c974243a351d5397
Raj162002 <ramkumarrajeevan16@gmail.com>
2024-03-10 22:09:19 +0530

    Person class Setting up the getters and setters using IntelliJ (shortcut=alt+insert)

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String email;
+
+    public static void main(String[] args){
+        Person myObj=new Person();
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
